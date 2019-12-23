8a064e3fb78b360f814ef6adeea5bd457327b597
zlocker <zlock030217@gmail.com>
2019-12-23 17:05:01 +0800

    定时器

---
+++ QtWidgetLearn/Demo_Timer/widget.cpp
@@ -0,0 +1,43 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QTime>
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    m_timer.setInterval(1000);
+    connect(&m_timer,SIGNAL(timeout()),this,SLOT(rgbColor()));
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::timerEvent(QTimerEvent *event)
+{
+    if(event->timerId()==timerID)
+        rgbColor();
+}
+
+void Widget::on_pushButton_clicked()
+{
+//    m_timer.start();
+    timerID = this->startTimer(500);
+}
+
+void Widget::on_pushButton_2_clicked()
+{
+//    m_timer.stop();
+    killTimer(timerID);
+}
+
+void Widget::rgbColor()
+{
+    QString bgColor = QString("background-color: rgb(%1,%2,%3)")
+            .arg(qrand()%255).arg(qrand()%255).arg(qrand()%255);
+    ui->label->setStyleSheet(bgColor);
+}
---
+++ QtWidgetLearn/Demo_Timer/widget.h
@@ -0,0 +1,31 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTimer>
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+protected:
+    void timerEvent(QTimerEvent *event);
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+    void rgbColor();
+private:
+    Ui::Widget *ui;
+    QTimer m_timer;     //方式1
+    int timerID;        //方式2
+};
+
+#endif // WIDGET_H
