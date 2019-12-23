41351234d4e98d5ac99c8dfa865278e7fad4ede2
zlocker <zlock030217@gmail.com>
2019-12-23 11:13:19 +0800

    QPainter绘图

---
+++ QtWidgetLearn/Demo_QPainter/mainwindow.h
@@ -0,0 +1,26 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void magicTime();
+protected:
+    bool eventFilter(QObject *watched, QEvent *event);
+virtual void paintEvent(QPaintEvent *event);
+private:
+    Ui::MainWindow *ui;
+
+};
+
+#endif // MAINWINDOW_H
