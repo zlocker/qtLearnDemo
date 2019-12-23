1c622045d2c4dbd08f562eaf2ade35bc6eddd288
zlocker <zlock030217@gmail.com>
2019-12-23 10:10:31 +0800

    Qt基础训练

---
+++ QtWidgetLearn/Demo_Basic/build-ex-alpha-blending-Desktop_Qt_5_7_0_MSVC2015_32bit-Debug/debug/moc_mywidget.cpp
@@ -0,0 +1,111 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mywidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../ex-alpha-blending/mywidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mywidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MyWidget_t {
+    QByteArrayData data[3];
+    char stringdata0[23];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MyWidget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MyWidget_t qt_meta_stringdata_MyWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "MyWidget"
+QT_MOC_LITERAL(1, 9, 12), // "selectButton"
+QT_MOC_LITERAL(2, 22, 0) // ""
+
+    },
+    "MyWidget\0selectButton\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MyWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   19,    2, 0x09 /* Protected */,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MyWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MyWidget *_t = static_cast<MyWidget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->selectButton(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MyWidget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_MyWidget.data,
+      qt_meta_data_MyWidget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MyWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MyWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MyWidget.stringdata0))
+        return static_cast<void*>(const_cast< MyWidget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int MyWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
